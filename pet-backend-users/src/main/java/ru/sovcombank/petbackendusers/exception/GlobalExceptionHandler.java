93a2a384b153f5763e8a8b25277798697d7232c3
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-11-17 18:21:01 +0300

    Добавлена обработка исключения БД с CONFLICT (409)

--- pet-backend-users/src/main/java/ru/sovcombank/petbackendusers/exception/GlobalExceptionHandler.java
+++ pet-backend-users/src/main/java/ru/sovcombank/petbackendusers/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package ru.sovcombank.petbackendusers.exception;
 
+import jakarta.validation.ConstraintViolation;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,12 +12,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
  * Глобальный обработчик исключений для управления ошибками в приложении.
  */
 @ControllerAdvice
+@Component
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InternalServerErrorException.class)
@@ -46,5 +52,25 @@ public class GlobalExceptionHandler {
         String errorMessage = "Некорректный запрос по полю: " + String.join(", ", invalidFields);
 
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> handleConflictException(ConflictException e) {
+        ConstraintViolationException cause = (ConstraintViolationException) e.getCause().getCause();
+        String message = cause.getMessage();
+        String regex = "Key \\((.*?)\\)=\\((.*?)\\)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(message);
+
+        String errorMessage = "";
+        while (matcher.find()) {
+            String field = matcher.group(1); // Поле
+            String value = matcher.group(2); // Значение
+            errorMessage = value + " с таким " + field + " уже зарегистрирован";
+        }
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.CONFLICT);
     }
 }
