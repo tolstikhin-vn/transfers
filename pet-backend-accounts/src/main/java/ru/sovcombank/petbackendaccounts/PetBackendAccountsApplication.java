9cb1978fd1c465cf47bbc300edb62921e6a6186e
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-11-21 18:08:35 +0300

    Точка входа

---
+++ pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/PetBackendAccountsApplication.java
@@ -0,0 +1,13 @@
+package ru.sovcombank.petbackendaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBackendAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBackendAccountsApplication.class, args);
+	}
+
+}
