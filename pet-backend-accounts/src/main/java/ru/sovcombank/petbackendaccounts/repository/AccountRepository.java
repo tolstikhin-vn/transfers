5a655e360e3d11233de7a2eac65f41206f98bbb8
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-11-23 21:05:46 +0300

    Новые методы для работы со счетами в БД

--- pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/repository/AccountRepository.java
+++ pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/repository/AccountRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sovcombank.petbackendaccounts.model.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByClientIdAndCur(Integer clientId, String cur);
+    Optional<List<Account>> findByClientId(Integer clientId);
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
