ed0ef51d1a00367913492245c1d0303e0b7a3ed2
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-11-23 20:57:23 +0300

    Созданы новые контрольные точки

--- pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/PetBackendAccountsApplication.java
+++ pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/PetBackendAccountsApplication.java
@@ -9,5 +9,4 @@ public class PetBackendAccountsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetBackendAccountsApplication.class, args);
 	}
-
 }
--- pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/controller/AccountController.java
+++ pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/controller/AccountController.java
@@ -3,12 +3,21 @@ package ru.sovcombank.petbackendaccounts.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sovcombank.petbackendaccounts.api.request.CreateAccountRequest;
+import ru.sovcombank.petbackendaccounts.api.request.UpdateBalanceRequest;
 import ru.sovcombank.petbackendaccounts.api.response.CreateAccountResponse;
+import ru.sovcombank.petbackendaccounts.api.response.DeleteAccountResponse;
+import ru.sovcombank.petbackendaccounts.api.response.GetAccountsResponse;
+import ru.sovcombank.petbackendaccounts.api.response.GetBalanceResponse;
+import ru.sovcombank.petbackendaccounts.api.response.UpdateBalanceResponse;
 import ru.sovcombank.petbackendaccounts.service.AccountService;
 
 /**
@@ -34,6 +43,55 @@ public class AccountController {
     @PostMapping("/new")
     public ResponseEntity<Object> createUser(@Valid @RequestBody CreateAccountRequest createAccountRequest) {
         CreateAccountResponse response = accountService.createAccount(createAccountRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Обрабатывает запрос на получение счетов клиента по id клиента.
+     *
+     * @param clientId Идентификатор клиента.
+     * @return Ответ с информацией о счетах.
+     */
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Object> getAccountsByClientId(@PathVariable String clientId) {
+        GetAccountsResponse response = accountService.getAccounts(clientId);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Обрабатывает запрос на удаление счета.
+     *
+     * @param accountNumber Номер счета.
+     * @return Ответ с результатом удаления счета.
+     */
+    @DeleteMapping("/{accountNumber}")
+    public ResponseEntity<Object> deleteAccount(@PathVariable String accountNumber) {
+        DeleteAccountResponse response = accountService.deleteAccount(accountNumber);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Обрабатывает запрос на получение баланса.
+     *
+     * @param accountNumber Номер счета.
+     * @return Ответ с балансом.
+     */
+    @GetMapping("/balance/{accountNumber}")
+    public ResponseEntity<Object> getBalance(@PathVariable String accountNumber) {
+        GetBalanceResponse response = accountService.getBalance(accountNumber);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Обрабатывает запрос на изменение/пополнение баланса.
+     *
+     * @param accountNumber        Номер счета.
+     * @param updateBalanceRequest Запрос на изменение баланса.
+     * @return Ответ с результатом изменения баланса.
+     */
+    @PutMapping("/balance/{accountNumber}")
+    public ResponseEntity<Object> updateUser(@PathVariable String accountNumber, @Valid @RequestBody UpdateBalanceRequest updateBalanceRequest) {
+        UpdateBalanceResponse response = accountService.updateBalance(accountNumber, updateBalanceRequest);
         return ResponseEntity.ok(response);
     }
 }
