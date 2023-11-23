0c289b748bec7c02745f202408a430d6dd3b16ea
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-11-23 21:02:21 +0300

    Новые мапперы для GetAccountsResponse, AccountDTO

--- pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/mapper/AccountMapper.java
+++ pet-backend-accounts/src/main/java/ru/sovcombank/petbackendaccounts/mapper/AccountMapper.java
@@ -4,7 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.sovcombank.petbackendaccounts.api.request.CreateAccountRequest;
+import ru.sovcombank.petbackendaccounts.api.response.GetAccountsResponse;
+import ru.sovcombank.petbackendaccounts.dto.AccountDTO;
 import ru.sovcombank.petbackendaccounts.model.Account;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class AccountMapper {
@@ -24,5 +29,34 @@ public class AccountMapper {
      */
     public Account toEntity(CreateAccountRequest createAccountRequest) {
         return modelMapper.map(createAccountRequest, Account.class);
+    }
+
+    /**
+     * Преобразует список сущностей счетов в GetAccountsResponse.
+     *
+     * @param accounts Список сущностей счетов.
+     * @param clientId ID клиента.
+     * @return ответ GetAccountsResponse с информацией о счетах.
+     */
+    public GetAccountsResponse toGetAccountsResponse(List<Account> accounts, String clientId) {
+        List<AccountDTO> accountDtos = accounts.stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
+
+        GetAccountsResponse response = new GetAccountsResponse();
+        response.setClientId(clientId);
+        response.setAccountNumbers(accountDtos);
+
+        return response;
+    }
+
+    /**
+     * Преобразует сущность счета в DTO.
+     *
+     * @param account Сущность счета.
+     * @return DTO счета.
+     */
+    public AccountDTO toDTO(Account account) {
+        return modelMapper.map(account, AccountDTO.class);
     }
 }
