9c1ad7a14316c139a84c312cd2b70ff44a3f3e25
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-12-15 16:15:49 +0300

    Интеграционное тестирование

---
+++ pet-backend-transfers/src/test/resources/sql/insert-transfer.sql
@@ -0,0 +1,3 @@
+INSERT INTO transfers (uuid, account_number_from, account_number_to, amount, cur, transaction_date_time)
+VALUES ('14e6edca-6319-4d4a-99fc-b951323f78b8', '4200933666961739', '4200810666632677', 900, '810',
+        '2023-12-13T18:25:25');
