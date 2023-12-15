4113a1b8db06a22b9b45a60cb65c0d9a1124c8ef
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-12-15 16:14:39 +0300

    Рефакторинг кода

--- pet-backend-transfers/src/main/java/ru/sovcombank/petbackendtransfers/model/api/response/GetAccountResponse.java
+++ pet-backend-transfers/src/main/java/ru/sovcombank/petbackendtransfers/model/api/response/GetAccountResponse.java
@@ -1,5 +1,6 @@
 package ru.sovcombank.petbackendtransfers.model.api.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,6 @@ public class GetAccountResponse {
     private String cur;
     private BigDecimal balance;
     private boolean isMain;
+    @JsonProperty(value = "isClosed")
     private boolean isClosed;
 }
